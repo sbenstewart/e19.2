7fd8cd87fc4956b4eb8e954dedfa82bdc7482f43
Ben Stewart <sbenstewart@gmail.com>
2018-08-09 23:31:11 +0530

    Temporaryy

--- e19.py
+++ e19.py
@@ -2,6 +2,6 @@
 import os
 import sys
 
-os.system("./new.command '%s' & python kit/new.py" % str(sys.argv[1]))
+os.system("python new.py '%s' & python kit/new.py" % str(sys.argv[1]))
 
 os.system("clear")
--- new.py
+++ new.py
@@ -10,6 +10,8 @@ number = os.popen('ps aux | pgrep "Microsoft PowerPoint" | wc -l')
 print number
 
 
-while number != 0:
-    number = os.popen('ps aux | pgrep "Microsoft PowerPoint" | wc -l').readlines()
+while number[0]-0 != 0:
+    number = os.popen('ps aux | pgrep "Microsoft PowerPoint" | wc -l').readline()
     print number[0]
+
+os.system("python killer.py")
