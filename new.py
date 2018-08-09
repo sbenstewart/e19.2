c8fe369e1565dadc5704851028f3a30d9892a780
Ben Stewart <sbenstewart@gmail.com>
2018-08-09 23:39:36 +0530

    Working complete.

--- new.py
+++ new.py
@@ -6,12 +6,22 @@ import subprocess
 
 os.system("open -a 'Microsoft PowerPoint.app' '%s'" % str(sys.argv[1]))
 
-number = os.popen('ps aux | pgrep "Microsoft PowerPoint" | wc -l')
-print number
+
+command = ["ps","aux","|","pgrep","Microsoft PowerPoint","|","wc","-l"]
+command = 'ps aux | pgrep "Microsoft PowerPoint" | wc -l'
+process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=None, shell=True)
+
+#Launch the shell command:
+output = process.communicate()
+
+#print output[0]
 
 
-while number[0]-0 != 0:
-    number = os.popen('ps aux | pgrep "Microsoft PowerPoint" | wc -l').readline()
-    print number[0]
+
+
+while int(output[0]) != 0:
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=None, shell=True)
+    output = process.communicate()
+    #print output[0]
 
 os.system("python killer.py")
