8b826aa1f51978bd3e5bfcae25dada7ff1155ad6
Ben Stewart <sbenstewart@gmail.com>
2018-08-22 20:37:10 +0530

    No turning back

--- e19.py
+++ e19.py
@@ -2,6 +2,6 @@
 import os
 import sys
 
-os.system("python checker.py '%s' & python kit.py" % str(sys.argv[1]))
+os.system("python new.py '%s' & python kit/new.py" % str(sys.argv[1]))
 
 os.system("clear")
